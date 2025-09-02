38c70c4b0c139512c7a62a32dd8ad07288885b0c
sarbajitnerp <sarbajit.sahoo@nirmalyalabs.com>
2025-09-02 19:00:35 +0530

    loading color changed

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
     return (
         <div className="flex h-screen w-full items-center justify-center">
             <div className="flex flex-col items-center">
-                <div className="h-12 w-12 animate-spin rounded-full border-4 border-emerald-500 border-t-transparent"></div>
+                <div className="h-12 w-12 animate-spin rounded-full border-4 border-fuchsia-500 border-t-transparent"></div>
                 <p className="mt-4 text-lg font-medium text-gray-600">Loading...</p>
             </div>
         </div>
